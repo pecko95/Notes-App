5fce1c1a0dbd3c2ada262a9c26e92882dece11b2
pecko95 <petar@codeart.mk>
2020-02-20 17:03:04 +0100

    Updated Users API route to include getting specific user and searching by username (for now)

--- backend/src/api/routes/users.js
+++ backend/src/api/routes/users.js
@@ -1,13 +1,45 @@
 import { Router } from "express";
 
+import User from "../../models/user";
+
 // Initialize the router
 const route = Router();
 
 const userRoutes = app => {
   app.use("/users", route);
 
-  route.get("/test", (req, res) => {
-    return res.send("<h1>TEST USER</h1>");
+  // Get all users
+  route.get("/", (req, res) => {
+    User.find({}).then(users => {
+      res.json(users.map(user => user.toJSON()));
+    })
+  })
+
+  // Get specific user
+  route.get("/:username", (req, res) => {
+    const username = req.params.username;
+
+    // Query the DB for the unique username and check if it exists
+    User.findOne({ username })
+      .then(user => {
+        if (user) {
+          res.json(user.toJSON());
+        } else {
+          res.status(404).json({
+            error: "User not found!"
+          })
+        }
+      })
+      .catch(err => console.log("Error", err.message))
+  })
+
+  // Search user by username ONLY - for now
+  route.get("/search/:username", (req, res) => {
+    const username = req.params.username;
+
+    User.find({ username }).then(users => {
+      res.json(users.map(user => user.toJSON()))
+    })
   })
 }
 

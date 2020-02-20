bb5a62fa8434eb6546dcc5143ba58c76ce10c755
pecko95 <petar@codeart.mk>
2020-02-20 13:25:31 +0100

    Changed 'startServer' function into async which waits for all loaders to finish

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import env from "./config/index";
 
-
-function startServer() {
+async function startServer() {
   const app = express();
+
+  // Wait for the loaders to be fully loaded and running,
+  // then use the imported expressApplication as the application to listen to
+  await require("./loaders").default({ expressApp: app })
 
   app.listen(env.PORT, (err: Error) => {
     if (err) {

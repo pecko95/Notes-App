8d1211138f6adf2b0c59d6fe71d7c3c51750ee33
pecko95 <petar@codeart.mk>
2020-02-27 13:50:01 +0100

    Export JWT_SECRET from ENV file

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -3,5 +3,6 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT,
-  MONGODB_URI: process.env.MONGODB_URI
+  MONGODB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.JWT_SECRET
 }

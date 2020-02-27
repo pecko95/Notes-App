4c694ef14402fc6477ba64987812928f2d284a96
pecko95 <petar@codeart.mk>
2020-02-27 13:49:41 +0100

    Issue a JWT if user logs in successfully

--- backend/src/api/routes/login.js
+++ backend/src/api/routes/login.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import User from "../../models/user";
+import jwt from "jsonwebtoken";
+import env from "../../config/index";
 
 const route = Router();
 
@@ -9,25 +11,48 @@ const loginRoute = app => {
   route.post("/", (req, res, next) => {
     // Get the username and password that were provided
     const { username, password } = req.body;
+    let result = {};
+    let status = 200;
 
     if (!username || !password) {
-      res.status(403).json({
-        error: "Please enter username and/or password."
-      });
+      status = 403;
+      result.status = status;
+      result.error = "Please enter username and/or password."
+      res.status(status).send(result);
     } else {
       // Check if the user exists and is authenticated
       User.authenticate(username, password, (err, user) => {
-        if (err || !user) {
-          
-          res.status(403).json({
-            error: "Wrong username or password!"
-          })
-
+        if(err) {
+          status = 500;
+          result.status = status;
+          result.error = `${err}`
+        } else if (!user) {
+          status = 401;
+          result.status = status;
+          result.error = "Authentication failed. Please provide matching username and password!"
         } else {
-          // Get data for specific user
-          // res.redirect(`/api/users/id/${username}`);
-          res.json(user.toJSON());
+          // If login passes
+          status = 200;
+
+          // Create the JWT
+          const payload = { username };
+
+          // Options - exipiry date, issued by etc.
+          const options = { expiresIn: "1h", issuer: "http://localhost:testing"};
+          
+          // JWT Secret from ENV
+          const secret = env.JWT_SECRET;
+
+          // Sign the token
+          const token = jwt.sign(payload, secret, options);
+
+          // Return the token details as a response
+          result.token = token;
+          result.status = status;
+          result.result = user;
         }
+
+        res.status(status).send(result);
       })
     }
   })

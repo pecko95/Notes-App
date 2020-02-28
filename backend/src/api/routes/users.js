cc1be7fe083fab381fa78073aa821747577d4f00
pecko95 <petar@codeart.mk>
2020-02-28 16:25:07 +0100

    Added route to update specific user

--- backend/src/api/routes/users.js
+++ backend/src/api/routes/users.js
@@ -16,6 +16,10 @@ const userRoutes = app => {
   route.get("/", validateJWT, (req, res) => {
     const payload = req.decoded;
 
+    // Clear result object and reset status on each request
+    status = 200;
+    result = {};
+
     // Check if payload exists and the users role
     if (payload && payload.user.role === "Admin") {
       User.find({}).then(users => {
@@ -33,6 +37,9 @@ const userRoutes = app => {
   route.get("/id/:id?", validateJWT, (req, res, next) => {
     const id = req.params.id;
     const payload = req.decoded;
+    // Clear result object and reset status on each request
+    status = 200;
+    result = {};
 
     if (payload && payload.user.role === "Admin") {
       // Check if id was passed as parameter
@@ -72,6 +79,9 @@ const userRoutes = app => {
   // Search for users - firstname, lastname and/or username
   route.get("/search/:searchParam?", validateJWT, (req, res) => {
     const searchParam = req.params.searchParam;
+    // Clear result object and reset status on each request
+    status = 200;
+    result = {};
     
     // Check if a search parameter was passed
     if (typeof searchParam != "undefined") {
@@ -111,6 +121,9 @@ const userRoutes = app => {
   // Get all notes that belong to a specific user
   route.get("/:id?/notes", validateJWT, (req, res, next) => {
     const id = req.params.id;
+    // Clear result object and reset status on each request
+    status = 200;
+    result = {};
 
     if (typeof id !== "undefined") {
       // Search trough the DB to find and return ALL notes that have the ID of the user's ID
@@ -130,6 +143,72 @@ const userRoutes = app => {
     } else {
       return next();
     }
+  })
+
+  // Update specific user
+  route.put("/:id/edit", validateJWT, (req, res, next) => {
+    const id = req.params.id;
+    const payload = req.decoded;
+    const { username, first_name, last_name } = req.body;
+
+    // Clear result object and reset status on each request
+    status = 200;
+    result = {};
+
+    if (!username || !first_name || !last_name) {
+      status = 400;
+      result.status = status;
+      result.error = "Please fill all required fields.";
+
+      res.status(status).send(result);
+    } else {
+      // If required values are sent check for role and if ID was passed as parameter
+      // Allow user details to be updated if:
+      // 1. Current user is Admin - can update every user details
+      // 2. Current logged in user tries to update his own details
+      if (payload && payload.user.role === "Admin" || payload && payload.user.id === id) {
+        // Check if user's ID is passed
+        if (!id) {
+          status = 401;
+          result.status = status;
+          result.error = "Provide user ID.";
+    
+          res.status(status).send(result);
+        } else {
+          User.findOneAndUpdate({ _id: id }, {
+            $set: {
+              username,
+              first_name,
+              last_name
+            }
+          }, (err, updatedUser) => {
+            if (err) {
+              status = 500;
+              result.status = status;
+              result.error = "Something went wrong. Can't update the user!"
+            } else if (!updatedUser) {
+              status = 400;
+              result.status = status;
+              result.error = "User does not exist!";
+            } else {
+              // If user exists
+              status = 200;
+              result.status = status;
+              result.message = "User updated successfully!";
+            }
+
+            res.status(status).send(result);
+          })
+        }
+      } else {
+        status = 401;
+        result.status = status;
+        result.error = "Not authorized.";
+  
+        res.status(status).send(result);
+      }
+    }
+
   })
 
 }

1df7959616ed58dfadabaa4b76d487aef64c81d3
pecko95 <petar@codeart.mk>
2020-03-03 14:51:25 +0100

    Added Context providers and consumers. Added initial logged in and logged out routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,75 @@
 import React from 'react';
+import { Route, Switch, Redirect, RouteComponentProps } from "react-router-dom";
+import { StaticContext, useLocation } from "react-router";
 import './scss/index.scss';
+import { toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { getCookie } from './utils/cookieHandling';
+import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
+import { UserContextConsumer } from './utils/UserContext';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>TEST</h2>
+  // Toastify global configuration
+  toast.configure({
+    position: "bottom-center",
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    autoClose: 2000
+  });
+
+  // Get token saved in cookie
+  const token = getCookie("notes-app_token");
+
+  const routes: {
+    path: string;
+    component: React.ComponentType<RouteComponentProps<any>> | React.ComponentType<any> |
+    { (props: RouteComponentProps<any, StaticContext, any>): React.Component };
+    title?: string;
+    adminRestriction?: boolean;
+  }[] = [
+    {
+      path: "/",
+      component: Home,
+      title: "Home"
+    }
+  ];
+
+  // Routes to use when not logged in
+  const notLoggedIn = (
+    <Switch>
+      <Route path="/login" exact component={Login} />
+
+      <Redirect to="/login" />
+    </Switch>
+  )
+
+  // Routes to use if logged in
+  const loggedIn = (
+    <div className="content">
+      <Switch>
+        { routes.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            exact
+            render={(props: RouteComponentProps) => (
+              <route.component {...props} />
+            )}
+          />
+        ))}
+      </Switch>
     </div>
+  )
+
+  return (
+    <UserContextConsumer>
+      {value => (
+        value?.isLoggedIn || token ? loggedIn : notLoggedIn
+      )}
+    </UserContextConsumer>
   );
 }
 
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './scss/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { UserContextProvider } from './utils/UserContext';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <UserContextProvider>
+      <div>
+        <App />
+      </div>
+    </UserContextProvider>
+  </BrowserRouter>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
